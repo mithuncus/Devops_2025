0e43ed9ebb6e81e7911145f182b7026ef30de97b
Madhan Sekhar <Madhancus@gmail.com>
2025-07-23 13:16:47 -0400

    am learning shell

---
+++ shellscript/02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Ramesh: Hello suresh, Good Morning "
+echo "Suresh: Hi Ramesh , Very Good Morning"
+echo "Ramesh: How are you Suresh?"
+echo "suresh: I am good Ramesh, How are you"
