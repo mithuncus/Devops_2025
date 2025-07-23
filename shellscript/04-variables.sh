10ae2638a407f4afe1430708c43aa281302d84ac
Madhan Sekhar <Madhancus@gmail.com>
2025-07-23 15:35:54 -0400

    am learning shell

--- shellscript/04-variables.sh
+++ shellscript/04-variables.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 DATE=$(date)
 CAL=$(cal)
+HOSTNAME=$(hostName)
+
+DISKFREE=$(df -hT)
+
 echo "date and time is $DATE"
 
 
 echo "this month calander $CAL"
+
+echo " hostname = $HOSTNAME"
+
+echo "diskfree $DISKFREE"
